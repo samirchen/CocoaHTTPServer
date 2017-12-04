0c397e37e19e5e580ad09ae228a0af9932050e33
qiufu <chenxuan.cx@alibaba-inc.com>
2017-12-04 20:39:48 +0800

    Add framework header.

---
+++ CocoaHTTPServer.h
@@ -0,0 +1,17 @@
+//
+//  CocoaHTTPServer.h
+//  CocoaHTTPServer
+//
+//  Created by SamirChen on 2017/8/10.
+//  Copyright © 2017年 SamirChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double CocoaHTTPServerVersionNumber;
+FOUNDATION_EXPORT const unsigned char CocoaHTTPServerVersionString[];
+
+#import <CocoaHTTPServer/HTTPServer.h>
+#import <CocoaHTTPServer/HTTPConnection.h>
+#import <CocoaHTTPServer/HTTPMessage.h>
+#import <CocoaHTTPServer/HTTPResponse.h>
